fa90411edb31ccbe36e7663ce5ef1ee032008098
Kyle Thomas <39631918+kthomas4031@users.noreply.github.com>
2021-06-03 15:17:24 -0400

    Created Random Corrupter

--- Corrupter.py
+++ Corrupter.py
@@ -1,5 +1,24 @@
 import pickle
+import numpy as np
+import random
 
-f = open("./Model_1_elu_weights.pickle", "rb")
+f = open("./Networks/Model_1_elu_weights.pickle", "rb")
 weights = pickle.load(f)
 
+def random_corrupt(layer):
+    for i in range(len(layer)):
+        chance = random.randrange(0,19)
+        if chance <= 1:
+            layer[i] = random.randrange(-2, 2)
+
+def recurWeights(layer):
+    if np.isscalar(layer[0]):
+        random_corrupt(layer)
+    else:
+        for j in layer:
+            recurWeights(j)
+
+
+recurWeights(weights)
+
+pickle.dump(weights, open("./RandomlyCorruptedNetworks/Model_1_elu_weights.pickle", "wb"))
