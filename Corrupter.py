0ccd5c7ca36c34430bbfa0c24e487b9d34b357ed
Kyle Thomas <39631918+kthomas4031@users.noreply.github.com>
2021-06-04 13:53:09 -0400

    Iterates through whole directory now

--- Corrupter.py
+++ Corrupter.py
@@ -1,24 +1,28 @@
 import pickle
 import numpy as np
 import random
+import os
 
-f = open("./Networks/Model_1_elu_weights.pickle", "rb")
-weights = pickle.load(f)
+directory = r'./Networks'
 
-def random_corrupt(layer):
-    for i in range(len(layer)):
-        chance = random.randrange(0,19)
-        if chance <= 1:
-            layer[i] = random.randrange(-2, 2)
+for filename in os.listdir(directory):
+    f = open("./Networks/%s"%filename, "rb")
+    weights = pickle.load(f)
 
-def recurWeights(layer):
-    if np.isscalar(layer[0]):
-        random_corrupt(layer)
-    else:
-        for j in layer:
-            recurWeights(j)
+    def random_corrupt(layer):
+        for i in range(len(layer)):
+            chance = random.randrange(0,19)
+            if chance <= 1:
+                layer[i] = random.randrange(-2, 2)
+
+    def recurWeights(layer):
+        if np.isscalar(layer[0]):
+            random_corrupt(layer)
+        else:
+            for j in layer:
+                recurWeights(j)
 
 
-recurWeights(weights)
+    recurWeights(weights)
 
-pickle.dump(weights, open("./RandomlyCorruptedNetworks/Model_1_elu_weights.pickle", "wb"))
+    pickle.dump(weights, open("./RandomlyCorruptedNetworks/%s" % filename, "wb"))
