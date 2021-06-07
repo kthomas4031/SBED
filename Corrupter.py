3170cbf6779b228ac0408a9e135fac329c0e58b2
Kyle Thomas <39631918+kthomas4031@users.noreply.github.com>
2021-06-07 13:38:26 -0400

    Update Corrupter.py

--- Corrupter.py
+++ Corrupter.py
@@ -3,25 +3,28 @@ import numpy as np
 import random
 import os
 
+
+def random_corrupt(layer):
+    for i in range(len(layer)):
+        chance = random.randrange(0, 19)
+        if chance <= 1:
+            layer[i] = random.randrange(-2, 2)
+
+
+def recurWeights(layer):
+    if np.isscalar(layer[0]):
+        random_corrupt(layer)
+    else:
+        for j in layer:
+            recurWeights(j)
+
+
 directory = r'./Networks'
 
 for filename in os.listdir(directory):
     f = open("./Networks/%s"%filename, "rb")
     weights = pickle.load(f)
-
-    def random_corrupt(layer):
-        for i in range(len(layer)):
-            chance = random.randrange(0,19)
-            if chance <= 1:
-                layer[i] = random.randrange(-2, 2)
-
-    def recurWeights(layer):
-        if np.isscalar(layer[0]):
-            random_corrupt(layer)
-        else:
-            for j in layer:
-                recurWeights(j)
-
+    weights[-1] = weights[-1].flatten()
 
     recurWeights(weights)
 
